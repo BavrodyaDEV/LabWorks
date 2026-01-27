be59c9baabf2e46dad131d9ec8fedc8976945752
BavrodyaDEV <nikitaznam12@yandex.ru>
2026-01-27 18:34:03 +0500

    Исправлены ещё ошибки в коде

--- LabWork1/Untitled1.py
+++ LabWork1/Untitled1.py
@@ -3,24 +3,24 @@ class tests: # Тестовый метод
     __y = 0
     __z = 0
     
-    def printer(text): # тестовая функция печатает передаваемое значение
+    def printer(self, text): # тестовая функция печатает передаваемое значение
         print(text)
     
-    def setX(num): # сеттеры для 3-х неизменяемых значений
+    def setX(self, num): # сеттеры для 3-х неизменяемых значений
         self.__x = num
         print("Изменён X")
-    def setY(num):
+    def setY(self, num):
         self.__y = num
         print("Изменён Y")
-    def setZ(num):
+    def setZ(self, num):
         self.__z = num
         print("Изменён Z")
 
-    def printVars():
-        print(self.__x, self.__y, self.__z)
+    def printVars(self):
+        print(f"{self.__x}, {self.__y}, {self.__z}")
 
 class testsChild(tests): # Дочерний класс
-    def __init__(x, y, z): # Конструктор сразу задающий значения
+    def __init__(self, x, y, z): # Конструктор сразу задающий значения
         self.setX(x)
         self.setY(y)
         self.setZ(z)
--- LabWork1/main.py
+++ LabWork1/main.py
@@ -2,4 +2,4 @@ import Untitled1 as mod
 
 tst = mod.testsChild(4, 6, 8)
 print("Значения: ")
-tst.printVars
+tst.printVars()
