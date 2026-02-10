0650cdac8eee73baf1f34953213a0c2d4466ee94
BavrodyaDEV <nikitaznam12@yandex.ru>
2026-02-10 10:10:47 +0500

    Лабораторная работа №3 тестирование чтения из Книги Excel

---
+++ LabWork3/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1;
+
+using ClosedXML.Excel;
+internal class Program
+{
+   
+    static void Main()
+    {
+        using var XLBook = new XLWorkbook("C:\\Users\\Redmi\\labWorks\\LabWork3\\ConsoleApp1\\bin\\Debug\\net10.0\\Задание 6_4.xlsx");
+        
+        var Sheet = XLBook.Worksheet("Задание 1");
+
+        int row = 1;
+
+        while (!Sheet.Cell($"A{row}").IsEmpty())
+        {
+            Console.WriteLine($"{Sheet.Cell($"A{row}").Value}");
+            ++row;
+        }
+    }
+}
