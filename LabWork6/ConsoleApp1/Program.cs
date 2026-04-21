6019a91da8692c6dc9f071d86cc50d32af43237f
Bavrodya <nikitaznam12@yandex.ru>
2026-04-21 10:40:04 +0500

    Добавлено 6-ое задание

---
+++ LabWork6/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    static void Main()
+    {
+        Regex pattern = new Regex(@"(\w*)рыб(\w*)");
+        Regex strBeginPattern = new Regex(@"^Мног\w*");
+        Regex strEndPattern = new Regex(@"\w*ять\?$");
+        Regex replacePattern = new Regex(@"\w*bober kurwa\w*");
+
+       
+      
+
+        string text = File.ReadAllText("text.txt");
+        
+
+        
+
+        string[] lines = File.ReadAllLines("text.txt");
+
+
+        foreach (string line in lines)
+        {
+            if(strBeginPattern.IsMatch(line))
+            {
+                Console.WriteLine(line);
+            }
+            if(strEndPattern.IsMatch(line))
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        Console.WriteLine(pattern.Matches(text).Count);
+        
+
+        StreamWriter writer = new StreamWriter("text.txt", false);
+
+        writer.Write(replacePattern.Replace(text, "Lorem Ipsum"));
+        writer.Close();
+        
+
+       
+        
+    }
+}
